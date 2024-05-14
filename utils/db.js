d39227d3f6f1dcef1047ef34cde4c821bddbc087
UmarMsani <umuhammad220@gmail.com>
2024-05-14 02:39:41 +0000

    Implemented authentication, file management, background processing

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,5 @@
 import { MongoClient } from 'mongodb';
 
-// Retrieve MongoDB connection parameters from environment variables
 const DB_HOST = process.env.DB_HOST || 'localhost';
 const DB_PORT = process.env.DB_PORT || 27017;
 const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
